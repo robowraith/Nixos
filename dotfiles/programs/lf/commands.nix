10df5384afe159f7d83ea2d7fd4062dde87a0b22
Joachim Hoss <joachim.hoss@42he.com>
2025-11-20 17:18:03 +0100

    Second try to correctly escap lf configuration commands

--- dotfiles/programs/lf/commands.nix
+++ dotfiles/programs/lf/commands.nix
@@ -1,8 +1,5 @@
 {
   programs.lf.commands = {
-    on-select = ''&{{
-      lf -remote "send $id set statfmt \"$(eza -ld --color=always "$f" | sed 's/\\/\\\\/g;s/"/\\"/g')\""
-    }}'';
     open = ''
       ''${{
         case $(file --mime-type "$(readlink -f $f)" -b) in
@@ -11,7 +8,7 @@
             text/*|application/json|inode/x-empty|application/x-subrip) $EDITOR $fx;;
         image/x-xcf) setsid -f gimp $f >/dev/null 2>&1 ;;
         image/svg+xml) display -- $f ;;
-        image/*) rotdir $f | grep -i "\.\(png\|jpg\|jpeg\|gif\|webp\|avif\|tif\|ico\)\( _large\)*$" |
+        image/*) rotdir $f | grep -i "\.\(png\|jpg\|jpeg\|gif\|webp\|avif\|tif\|ico\)\(_large\)*$" |
             setsid -f nsxiv -aio 2>/dev/null | while read -r file; do
                 [ -z "$file" ] && continue
                 lf -remote "send select \"$file\""
@@ -26,14 +23,114 @@
             *) for f in $fx; do setsid -f $OPENER $f >/dev/null 2>&1; done;;
         esac
     }}'';
+    
     mkdir = ''
       ''${{
         mkdir -p "$@"
       }}'';
+    
     touch = ''
       ''${{
         touch "$@"
       }}'';
+    
+    extract = ''
+      ''${{
+        clear; tput cup $(($(tput lines)/3)); tput bold
+        set -f
+        printf "%s\n\t" "$fx"
+        printf "extract?[y/N]"
+        read ans
+        [ $ans = "y" ] && {
+          case $fx in
+            *.tar.bz2)   tar xjf $fx     ;;
+            *.tar.gz)    tar xzf $fx     ;;
+            *.bz2)       bunzip2 $fx     ;;
+            *.rar)       unrar e $fx     ;;
+            *.gz)        gunzip $fx      ;;
+            *.tar)       tar xf $fx      ;;
+            *.tbz2)      tar xjf $fx     ;;
+            *.tgz)       tar xzf $fx     ;;
+            *.zip)       unzip $fx       ;;
+            *.Z)         uncompress $fx  ;;
+            *.7z)        7z x $fx        ;;
+            *.tar.xz)    tar xf $fx      ;;
+          esac
+        }
+    }}'';
+    
+    delete = ''
+      ''${{
+        clear; tput cup $(($(tput lines)/3)); tput bold
+        set -f
+        printf "%s\n\t" "$fx"
+        printf "delete?[y/N]"
+        read ans
+        [ $ans = "y" ] && rm -rf -- $fx
+    }}'';
+    
+    moveto = ''
+      ''${{
+        clear; tput cup $(($(tput lines)/3)); tput bold
+        set -f
+        clear; echo "Move to where?"
+        dest="$(sed -e 's/\s*#.*//' -e '/^$/d' -e 's/^\S*\s*//' ''${XDG_CONFIG_HOME:-$HOME/.config}/shell/bm-dirs | fzf | sed 's|~|$HOME|')" &&
+        for x in $fx; do
+          eval mv -iv \"$x\" \"$dest\"
+        done &&
+        notify-send "🚚 File(s) moved." "File(s) moved to $dest."
+    }}'';
+    
+    copyto = ''
+      ''${{
+        clear; tput cup $(($(tput lines)/3)); tput bold
+        set -f
+        clear; echo "Copy to where?"
+        dest="$(sed -e 's/\s*#.*//' -e '/^$/d' -e 's/^\S*\s*//' ''${XDG_CONFIG_HOME:-$HOME/.config}/shell/bm-dirs | fzf | sed 's|~|$HOME|')" &&
+        for x in $fx; do
+          eval cp -ivr \"$x\" \"$dest\"
+        done &&
+        notify-send "📋 File(s) copied." "File(s) copies to $dest."
+    }}'';
+    
+    bulkrename = ''
+      ''${{
+        tmpfile_old="$(mktemp)"
+        tmpfile_new="$(mktemp)"
+
+        [ -n "$fs" ] && fs=$(basename -a $fs) || fs=$(ls)
+
+        echo "$fs" > "$tmpfile_old"
+        echo "$fs" > "$tmpfile_new"
+        $EDITOR "$tmpfile_new"
+
+        [ "$(wc -l < "$tmpfile_old")" -eq "$(wc -l < "$tmpfile_new")" ] || { rm -f "$tmpfile_old" "$tmpfile_new"; exit 1; }
+
+        paste "$tmpfile_old" "$tmpfile_new" | while IFS="$(printf '\t')" read -r src dst
+        do
+            [ "$src" = "$dst" ] || [ -e "$dst" ] || mv -- "$src" "$dst"
+        done
+
+        rm -f "$tmpfile_old" "$tmpfile_new"
+        lf -remote "send $id unselect"
+    }}'';
+    
+    edit-config = ''
+      ''${{
+        $EDITOR ~/.config/lf/lfrc
+        lf -remote "send $id source ~/.config/lf/lfrc"
+    }}'';
+    
+    z = ''%{{
+      result="$(zoxide query --exclude "$PWD" "$@" | sed 's/\\/\\\\/g;s/"/\\"/g')"
+      lf -remote "send $id cd \"$result\""
+    }}'';
+    
+    zi = ''
+      ''${{
+        result="$(zoxide query -i | sed 's/\\/\\\\/g;s/"/\\"/g')"
+        lf -remote "send $id cd \"$result\""
+    }}'';
   };
 }
 
--- dotfiles/programs/lf/keymap.nix
+++ dotfiles/programs/lf/keymap.nix
@@ -1,42 +1,53 @@
 {
   programs.lf.keybindings = {
-    A = ":rename; cmd-end";
+    # Config editing
+    C = "edit-config";
+    
+    # File selection and navigation
+    "<c-f>" = "$lf -remote \"send $id select \\\"$(fzf)\\\"\"";
+    J = "$lf -remote \"send $id cd $(sed -e 's/\\s*#.*//' -e '/^$/d' -e 's/^\\S*\\s*//' ''${XDG_CONFIG_HOME:-$HOME/.config}/shell/bm-dirs | fzf)\"";
+    gh = "";
+    g = "top";
+    "<c-g>" = ":fzf_search";
+    
+    # File operations
     D = "delete";
     E = "extract";
-    gh = "cd ~";
-    n = "push :touch<space>''<left>";
-    N = "push :mkdir<space>''<left>";
+    N = "push :mkdir<space>\"\"<left>";
+    n = "push :touch<space>\"\"<left>";
     
- #    <c-f> $lf -remote "send $id select \"$(fzf)\""
- # J $lf -remote "send $id cd $(sed -e 's/\s*#.*//' -e '/^$/d' -e 's/^\S*\s*//' ${XDG_CONFIG_HOME:-$HOME/.config}/shell/bm-dirs | fzf)"
- # g top
- # <c-g> :fzf_search
- # N push :mkdir<space>""<left>
- # n push :touch<space>""<left>
- # <c-r> reload
- # <c-s> set hidden!
- # <enter> shell
- # x $$f
- # X !$f
- # o &mimeopen "$f"
- # O $mimeopen --ask "$f"
- # <c-z> z
- # <c-i> zi
-
- # A :rename; cmd-end # at the very end
- # r push A<c-u> # new rename
- # I :rename; cmd-home # at the very beginning
- # i :rename # before extension
- # a :rename; cmd-right # after extension
- # B bulkrename
-
- # <c-e> down
- # <c-y> up
- # V push :!nvim<space>
-
- # W $setsid -f $TERMINAL >/dev/null 2>&1
-
- # Y $printf "%s" "$fx" | xclip -selection clipboard
+    # Refresh and toggle
+    "<c-r>" = "reload";
+    "<c-s>" = "set hidden!";
     
+    # Shell commands
+    "<enter>" = "shell";
+    x = "$$f";
+    X = "!$f";
+    o = "&mimeopen \"$f\"";
+    O = "$mimeopen --ask \"$f\"";
+    
+    # Zoxide integration
+    "<c-z>" = "z";
+    "<c-i>" = "zi";
+    
+    # Renaming
+    A = ":rename; cmd-end";  # at the very end
+    r = "push A<c-u>";  # new rename
+    I = ":rename; cmd-home";  # at the very beginning
+    i = ":rename";  # before extension
+    a = ":rename; cmd-right";  # after extension
+    B = "bulkrename";
+    
+    # Navigation
+    "<c-e>" = "down";
+    "<c-y>" = "up";
+    
+    # Editor and terminal
+    V = "push :!nvim<space>";
+    W = "$setsid -f $TERMINAL >/dev/null 2>&1";
+    
+    # Clipboard
+    Y = "$printf \"%s\" \"$fx\" | xclip -selection clipboard";
   };
 }
