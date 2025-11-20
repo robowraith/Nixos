7cb4955937d8d2f9fce0401eb351f6d11f3fd907
Joachim Hoss <joachim.hoss@42he.com>
2025-11-20 11:52:09 +0100

    Set system-shell to German layout

--- system/configuration.nix
+++ system/configuration.nix
@@ -43,11 +43,11 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "de_DE.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "de";
+    useXkbConfig = false; # do not use xkb.options in TTY; keep keyMap as simple console mapping
+  };
 
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
