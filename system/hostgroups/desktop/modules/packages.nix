235e7a11aefbe802eab9c04b90857093b972ee2a
Joachim Hoss <joachim.hoss@42he.com>
2025-12-10 23:03:27 +0100

    move signal-desktop to desktop hostgroup

--- system/hostgroups/desktop/modules/packages.nix
+++ system/hostgroups/desktop/modules/packages.nix
@@ -1,14 +1,13 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     # Core utilities
-    feh
 
     # # Wayland
     # wayland
     # wayland-utils
     # wl-clipboard
 
-    # Terminal
-    kitty
+    # Other apps
+    signal-desktop
   ];
 }
