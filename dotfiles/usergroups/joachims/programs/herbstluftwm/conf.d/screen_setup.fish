a404c6677ada1be38ce89b49e0eb341b7c4b4246
Joachim Hoss <robowraith@gmail.com>
2025-12-16 14:13:57 +0100

    Fix typos

--- dotfiles/usergroups/joachims/programs/herbstluftwm/conf.d/screen_setup.fish
+++ dotfiles/usergroups/joachims/programs/herbstluftwm/conf.d/screen_setup.fish
@@ -1,10 +1,14 @@
 #! /usr/bin/env fish
 
+function hc
+    herbstclient $argv
+end
+
 # Screen setup
 switch $AUTORANDR_CURRENT_PROFILE
     case Office_4K
         hc and , \
-            set_monitors 1600x560+1120+1600 1120x2160+0+0 1600x1600+1120+0 1120x2160+2780+0 , \
+            set_monitors 1600x560+1120+1600 1120x2160+0+0 1600x1600+1120+0 1120x2160+2720+0 , \
             rename_monitor 0 down , \
             rename_monitor 1 left , \
             rename_monitor 2 main , \
@@ -12,4 +16,4 @@ switch $AUTORANDR_CURRENT_PROFILE
 end
 
 # Wallpaper
-/usr/bin/feh --no-fehbg --bg-fill ~/wallpaper/BladeRunnerWP.jpg
+feh --no-fehbg --bg-fill ~/Bilder/Wallpaper/BladeRunnerWP.jpg
