b6582b05c1913b83b0f52201159e2a20596dd08f
Joachim Hoss <robowraith@gmail.com>
2025-12-20 16:15:29 +0100

    Add some Vivaldi extensions

--- dotfiles/usergroups/joachims/programs/vivaldi/vivaldi.nix
+++ dotfiles/usergroups/joachims/programs/vivaldi/vivaldi.nix
@@ -2,4 +2,22 @@
   home.packages = with pkgs; [
     vivaldi
   ];
+
+  programs.vivaldi = {
+    enable = true;
+    extensions = [
+      # Cookie AutoDelete
+      # https://chromewebstore.google.com/detail/cookie-autodelete/fhcgjolkccmbidfldomjliifgaodjagh
+      { id = "fhcgjolkccmbidfldomjliifgaodjagh"; }
+      # I still don`t care about cookies
+      # https://chromewebstore.google.com/detail/i-still-dont-care-about-c/edibdbjcniadpccecjdfdjjppcpchdlm
+      { id = "edibdbjcniadpccecjdfdjjppcpchdlm";}
+      # KeepAssXC Browser
+      # https://chromewebstore.google.com/detail/keepassxc-browser/oboonakemofpalcgghocfoadofidjkkk
+      { id = "oboonakemofpalcgghocfoadofidjkkk"; }
+      # Vimium
+      # https://chromewebstore.google.com/detail/vimium/dbepggeogbaibhgnhhndojpepiihcmeb
+      { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; }
+    ];
+  };
 }
