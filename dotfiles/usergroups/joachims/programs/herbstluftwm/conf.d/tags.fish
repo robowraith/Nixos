3a349d554d5bbd26d5e0359ced8284524d8385fd
Joachim Hoss <joachim.hoss@42he.com>
2025-12-21 02:01:49 +0100

    Correct tag name

--- dotfiles/usergroups/joachims/programs/herbstluftwm/conf.d/tags.fish
+++ dotfiles/usergroups/joachims/programs/herbstluftwm/conf.d/tags.fish
@@ -12,7 +12,7 @@ hc and , \
     try add 0 , \
     rename 0 down , \
     new_attr string tags.0.my_monitor , \
-    set_attr tags.0.my_monitor 0
+    set_attr tags.0.my_monitor down
 
 hc and , \
     add 1 , \
