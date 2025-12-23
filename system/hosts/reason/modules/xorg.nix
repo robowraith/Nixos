afff54822c618c2511afe43c578ea85a9672391d
Joachim Hoss <joachim.hoss@42he.com>
2025-12-23 16:12:50 +0100

    Correct statix warnings

--- system/hosts/reason/modules/xorg.nix
+++ system/hosts/reason/modules/xorg.nix
@@ -3,25 +3,27 @@ _: {
   # Display Manager
   # ============================================================================
 
-  services.displayManager = {
-    ly = {
-      enable = true;
-      settings = {
-        animation = "matrix";
-        clock = "%c";
-        tty = 1;
+  services = {
+    displayManager = {
+      ly = {
+        enable = true;
+        settings = {
+          animation = "matrix";
+          clock = "%c";
+          tty = 1;
+        };
       };
     };
-  };
 
-  services.xserver = {
-    enable = true;
-    windowManager.herbstluftwm.enable = true;
-    xkb = {
-      layout = "de";
-      variant = "nodeadkeys";
+    xserver = {
+      enable = true;
+      windowManager.herbstluftwm.enable = true;
+      xkb = {
+        layout = "de";
+        variant = "nodeadkeys";
+      };
     };
-  };
 
-  services.libinput.enable = true;
+    libinput.enable = true;
+  };
 }
