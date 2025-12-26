65269323cf73b70ce56cbbc82bfc624de145637d
Joachim Hoss <robowraith@gmail.com>
2025-12-26 17:45:09 +0100

    Add extensions to Chromium

--- dotfiles/usergroups/joachims/programs/chromium/chromium.nix
+++ dotfiles/usergroups/joachims/programs/chromium/chromium.nix
@@ -2,5 +2,19 @@
   programs.chromium = {
     enable = true;
     package = pkgs.ungoogled-chromium;
+    extensions = [
+      # Cookie AutoDelete
+      # https://chromewebstore.google.com/detail/cookie-autodelete/fhcgjolkccmbidfldomjliifgaodjagh
+      {id = "fhcgjolkccmbidfldomjliifgaodjagh";}
+      # I still don`t care about cookies
+      # https://chromewebstore.google.com/detail/i-still-dont-care-about-c/edibdbjcniadpccecjdfdjjppcpchdlm
+      {id = "edibdbjcniadpccecjdfdjjppcpchdlm";}
+      # KeePassXC Browser
+      # https://chromewebstore.google.com/detail/keepassxc-browser/oboonakemofpalcgghocfoadofidjkkk
+      {id = "oboonakemofpalcgghocfoadofidjkkk";}
+      # Vimium
+      # https://chromewebstore.google.com/detail/vimium/dbepggeogbaibhgnhhndojpepiihcmeb
+      {id = "dbepggeogbaibhgnhhndojpepiihcmeb";}
+    ];
   };
 }
