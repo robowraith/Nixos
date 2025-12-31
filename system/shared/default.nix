378f3e46b367086a80890c8ded9cf2847fc1214e
Joachim Hoss <robowraith@gmail.com>
2025-12-31 16:23:26 +0100

    Revert "Temporarily disable substituters because of problems"
    This reverts commit be6c96a41e15817ce5077bf036231b1e3abe7482.

--- system/shared/default.nix
+++ system/shared/default.nix
@@ -21,8 +21,8 @@
     settings = {
       experimental-features = ["nix-command" "flakes"];
       auto-optimise-store = true;
-      # substituters = ["https://cache.nixos.org/" "https://attic.xuyh0120.win/lantian" "https://cache.garnix.io"];
-      # trusted-public-keys = ["lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
+      substituters = ["https://cache.nixos.org/" "https://attic.xuyh0120.win/lantian" "https://cache.garnix.io"];
+      trusted-public-keys = ["lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
     };
     # Automatic garbage collection
     gc = {
