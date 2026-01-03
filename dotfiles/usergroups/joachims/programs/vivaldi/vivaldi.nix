fbd6a23d7593b3e6af6625bd38dca50625905378
Joachim Hoss <robowraith@gmail.com>
2026-01-03 16:59:09 +0100

    Make Vivaldi the default browser

--- dotfiles/usergroups/joachims/programs/vivaldi/vivaldi.nix
+++ dotfiles/usergroups/joachims/programs/vivaldi/vivaldi.nix
@@ -20,4 +20,15 @@
       {id = "dbepggeogbaibhgnhhndojpepiihcmeb";}
     ];
   };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "vivaldi-stable.desktop";
+      "x-scheme-handler/http" = "vivaldi-stable.desktop";
+      "x-scheme-handler/https" = "vivaldi-stable.desktop";
+      "x-scheme-handler/about" = "vivaldi-stable.desktop";
+      "x-scheme-handler/unknown" = "vivaldi-stable.desktop";
+    };
+  };
 }
