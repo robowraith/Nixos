a9a8d0394781496176e917224302953b98035b57
Joachim Hoss <robowraith@gmail.com>
2026-01-10 18:46:50 +0100

    Remove "down" monitor and split main monitor 4/1

--- dotfiles/usergroups/joachims/programs/herbstluftwm/conf.d/screen_setup.fish
+++ dotfiles/usergroups/joachims/programs/herbstluftwm/conf.d/screen_setup.fish
@@ -8,11 +8,10 @@ end
 switch $AUTORANDR_CURRENT_PROFILE
     case Office_4K
         hc and , \
-            set_monitors 1600x560+1120+1600 1120x2160+0+0 1600x1600+1120+0 1120x2160+2720+0 , \
-            rename_monitor 0 down , \
-            rename_monitor 1 left , \
-            rename_monitor 2 main , \
-            rename_monitor 3 right
+            set_monitors 1120x2160+0+0 1600x2160+1120+0 1120x2160+2720+0 , \
+            rename_monitor 0 left , \
+            rename_monitor 1 main , \
+            rename_monitor 2 right
 end
 
 # Wallpaper
