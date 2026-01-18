f360696acb673f17763e1435b32f885960b0ef8e
Joachim Hoss <robowraith@gmail.com>
2026-01-18 17:16:48 +0100

    Add persisten SSH tunnels to ac4 and dc1

---
+++ dotfiles/users/jhoss/programs/ssh-tunnels/ssh-tunnels.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }:
+
+{
+  systemd.user.services = {
+    ssh-tunnel-dc1 = {
+      Unit = {
+        Description = "SSH Tunnel to dc1 Kubernetes API";
+        After = [ "network.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.openssh}/bin/ssh -N -L 64431:localhost:6443 joachim@dc1";
+        Restart = "always";
+        RestartSec = "10";
+        StartLimitIntervalSec = "0";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+    };
+
+    ssh-tunnel-ac4 = {
+      Unit = {
+        Description = "SSH Tunnel to ac4 Kubernetes API";
+        After = [ "network.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.openssh}/bin/ssh -N -L 64430:localhost:6443 joachim@ac4";
+        Restart = "always";
+        RestartSec = "10";
+        StartLimitIntervalSec = "0";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+    };
+  };
+}
