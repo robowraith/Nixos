c56f2c82d58f292e777e62af25645901f51bc695
Joachim Hoss <robowraith@gmail.com>
2026-01-19 15:54:22 +0100

    Clone puppet_files repo

--- dotfiles/users/jhoss/modules/code.nix
+++ dotfiles/users/jhoss/modules/code.nix
@@ -1,12 +1,20 @@
 { pkgs, lib, ... }:
 
 {
-  home.activation.cloneAnsibleRepo = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  home.activation.cloneCodeRepos = lib.hm.dag.entryAfter ["writeBoundary"] ''
     $DRY_RUN_CMD mkdir -p "$HOME/code"
+    export GIT_SSH_COMMAND="${pkgs.openssh}/bin/ssh"
+
+    # Ansible
     ANSIBLE_DIR="$HOME/code/ansible"
     if [ ! -d "$ANSIBLE_DIR/.git" ]; then
-      export GIT_SSH_COMMAND="${pkgs.openssh}/bin/ssh"
       $DRY_RUN_CMD ${pkgs.git}/bin/git clone git@git.42he.com:devops/ansible.git "$ANSIBLE_DIR"
+    fi
+
+    # Puppet Files
+    PUPPET_DIR="$HOME/code/puppet_files"
+    if [ ! -d "$PUPPET_DIR/.git" ]; then
+      $DRY_RUN_CMD ${pkgs.git}/bin/git clone git@git.42he.com:devops/puppet_files.git "$PUPPET_DIR"
     fi
   '';
 }
