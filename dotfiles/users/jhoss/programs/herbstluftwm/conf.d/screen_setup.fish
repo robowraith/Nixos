bf0aadd4d9fed2abbcfcd7dd75d8bb192ff7a476
Joachim Hoss <robowraith@gmail.com>
2026-01-21 15:40:42 +0100

    Add screen setups for Deepthought

--- dotfiles/users/jhoss/programs/herbstluftwm/conf.d/screen_setup.fish
+++ dotfiles/users/jhoss/programs/herbstluftwm/conf.d/screen_setup.fish
@@ -6,13 +6,34 @@ end
 
 # Screen setup
 switch $AUTORANDR_CURRENT_PROFILE
-    case Office_4K
+    case Office_4K_*
         hc and , \
             set_monitors 1120x2160+0+0 1600x2160+1120+0 1120x2160+2720+0 , \
             rename_monitor 0 left , \
             rename_monitor 1 main , \
             rename_monitor 2 right
+
+    case Gaming_1080p
+        hc and , \
+            set_monitors 1920x1080+0+0 , \
+            rename_monitor 0 main
+
+    case Gaming_4K
+        hc and , \
+            set_monitors 3840x2160+0+0 , \
+            rename_monitor 0 main
+
+    case Notebook_1200
+        hc and , \
+            set_monitors 1920x1200+0+0 , \
+            rename_monitor 0 main
+
+    case Notebook_USBC
+        hc and , \
+            set_monitors 1920x1200+0+0 1920x1200+1920+0 , \
+            rename_monitor 0 main , \
+            rename_monitor 1 up
 end
 
 # Wallpaper
-feh --no-fehbg --bg-fill ~/Bilder/Wallpaper/Rechner/BladeRunnerWP.jpg
+feh --no-fehbg --bg-fill ~/Bilder/Wallpaper/Rechner/Köln.jpg
