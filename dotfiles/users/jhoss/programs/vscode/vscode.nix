d9c6d0449d565f5e56167e077dbc60c032170e56
Joachim Hoss <robowraith@gmail.com>
2026-01-28 16:03:42 +0100

    Add some VSCode settings

---
+++ dotfiles/users/jhoss/programs/vscode/vscode.nix
@@ -0,0 +1,30 @@
+{pkgs, lib, ...}: {
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions;
+      [
+        redhat.ansible
+        redhat.vscode-yaml
+        samuelcolvin.jinjahtml
+        ms-vscode-remote.remote-containers
+      ]
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "vscode-helix-emulation";
+          publisher = "jasew";
+          version = "0.7.0";
+          sha256 = "1diwv1q1z8kkx9v0jzzfb5sa5v9825dx872di9k0px66fmb8i341";
+        }
+      ];
+
+    userSettings = {
+      # Font configuration
+      "editor.fontFamily" = lib.mkForce "'FiraCode Nerd Font', 'Droid Sans Mono', 'monospace', monospace";
+      "editor.fontLigatures" = true;
+
+      # Disable RedHat telemetry
+      "ansible.lightspeed.enabled" = false;
+      "redhat.telemetry.enabled" = false;
+    };
+  };
+}
