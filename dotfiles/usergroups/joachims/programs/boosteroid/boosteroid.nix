c5586bb8c539cbd50613c6789446286b6440ff4f
Joachim Hoss <robowraith@gmail.com>
2026-01-31 01:35:28 +0100

    Install Boosteroid

---
+++ dotfiles/usergroups/joachims/programs/boosteroid/boosteroid.nix
@@ -0,0 +1,81 @@
+{pkgs, lib, ...}: let
+  boosteroid = pkgs.stdenv.mkDerivation rec {
+    pname = "boosteroid";
+    version = "2025-11-06";
+
+    src = ./boosteroid-install-x64.deb;
+
+    nativeBuildInputs = [
+      pkgs.dpkg
+      pkgs.autoPatchelfHook
+      pkgs.makeWrapper
+    ];
+
+    buildInputs = with pkgs; [
+      alsa-lib
+      pulseaudio
+      libva
+      libvdpau
+      numactl
+      libglvnd
+      xorg.libX11
+      xorg.libXfixes
+      xorg.libXi
+      xorg.libxcb
+      xorg.libXrandr
+      xorg.libXrender
+      xorg.libXext
+      xorg.libXcursor
+      xorg.libXinerama
+      xorg.libXdamage
+      xorg.libXcomposite
+      xorg.libxkbfile
+      xorg.libXau
+      xorg.libXdmcp
+      xorg.libXxf86vm
+      xorg.libXScrnSaver
+      xorg.libXtst
+      xorg.xcbutil
+      xorg.xcbutilimage
+      xorg.xcbutilkeysyms
+      xorg.xcbutilrenderutil
+      xorg.xcbutilwm
+      xcbutilxrm
+      libxkbcommon
+      wayland
+      dbus
+      fontconfig
+      freetype
+      pcre2
+      zlib
+      xz
+      libdrm
+      udev
+    ];
+
+    unpackPhase = "dpkg-deb -x $src $out";
+
+    installPhase = ''
+      runHook preInstall
+
+      substituteInPlace $out/usr/share/applications/Boosteroid.desktop \
+        --replace "/opt/BoosteroidGamesS.R.L./bin/Boosteroid" "boosteroid" \
+        --replace "/usr/share/icons/Boosteroid/icon.svg" "$out/usr/share/icons/Boosteroid/icon.svg"
+
+      mkdir -p $out/bin
+      ln -s $out/opt/BoosteroidGamesS.R.L./bin/Boosteroid $out/bin/boosteroid
+
+      runHook postInstall
+    '';
+
+    meta = with lib; {
+      description = "Boosteroid cloud gaming client";
+      homepage = "https://boosteroid.com/";
+      platforms = ["x86_64-linux"];
+      sourceProvenance = with sourceTypes; [binaryNativeCode];
+      license = licenses.unfreeRedistributable;
+    };
+  };
+in {
+  home.packages = [boosteroid];
+}
