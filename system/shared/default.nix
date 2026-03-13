0d846e4261d989ec79f0723800d03a1dda46cfcf
Joachim Hoss <robowraith@gmail.com>
2026-03-13 14:57:31 +0100

    Add devenv substituters

--- system/shared/default.nix
+++ system/shared/default.nix
@@ -29,12 +29,16 @@
         "https://attic.xuyh0120.win/lantian"
         "https://cache.garnix.io"
         "https://vicinae.cachix.org"
+        "https://nixpkgs-python.cachix.org"
+        "https://devenv.cachix.org"
       ];
       trusted-public-keys = [
         "nix-cachyos.cachix.org-1:89qeSnd5K74f3O0oK78Vpbeo68o5v3M02fVpL9l3Lq0="
         "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
+        "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       ];
     };
     # Automatic garbage collection
