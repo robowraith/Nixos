e5c3073e9151b179ec5fcc360d1e0d3fb03efb2b
Joachim Hoss <robowraith@gmail.com>
2026-05-11 16:42:34 +0200

    Include ipv6 DNS server at pihole

--- system/hostgroups/home/modules/networking.nix
+++ system/hostgroups/home/modules/networking.nix
@@ -6,7 +6,7 @@ _: {
   networking = {
     networkmanager.enable = true;
     defaultGateway = "192.168.1.1";
-    nameservers = ["192.168.1.3"];
+    nameservers = ["192.168.1.3" "2003:cc:b706:fbeb:3ea8:2aff:fea0:5161"];
 
     # Firewall
     firewall = {
