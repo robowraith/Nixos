75cbf262ee5c431657b5e4c549e6b397b012b120
Joachim Hoss <robowraith@gmail.com>
2026-05-12 14:18:52 +0200

    Remove network changes again

--- system/hostgroups/home/modules/networking.nix
+++ system/hostgroups/home/modules/networking.nix
@@ -4,9 +4,10 @@ _: {
   # ============================================================================
 
   networking = {
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+    };
     defaultGateway = "192.168.1.1";
-    nameservers = ["192.168.1.3" "2003:cc:b706:fbeb:3ea8:2aff:fea0:5161"];
 
     # Firewall
     firewall = {
