5f4e156bd1d15630159d67de4af53a71fcacb432
Joachim Hoss <robowraith@gmail.com>
2026-08-10 16:00:07 +0200

    Correct DHCP reservation location in networking.nix comment
    The .111 pin is a dnsmasq static lease on Pi-hole (wintermute), not a
    reservation on the router as previously noted.

--- system/hosts/reason/modules/networking.nix
+++ system/hosts/reason/modules/networking.nix
@@ -6,8 +6,8 @@
   networking = {
     hostName = hostname;
 
-    # IP via DHCP (NetworkManager); pin 192.168.1.111 with a DHCP
-    # reservation in the router
+    # IP via DHCP (NetworkManager); pin 192.168.1.111 with a static
+    # DHCP lease in Pi-hole (dnsmasq dhcp-host on wintermute)
 
     # Firewall
     firewall.allowedTCPPorts = [];
